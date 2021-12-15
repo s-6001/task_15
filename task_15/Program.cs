a7b60c47125aff569acb83902e4ea0a802b7b804
Эдуард Шамсиев <s-6001@yandex.ru>
2021-12-15 19:15:37 +0500

    Добавьте файлы проекта.

---
+++ task_15/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        interface ISeries
+        {
+            void setStart(int x);
+            int getNext();
+            void reset();
+        }
+        class ArithProgression : ISeries
+        {
+            int firstNumber = 2;
+            int step = 3;
+            int next;
+            public int getNext(int f, int s)
+            {
+                next = firstNumber+step;
+                return next;
+            }
+
+            public void reset()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void setStart(int x)
+            {
+                
+            }
+        }
+    }
+}
