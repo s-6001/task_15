3bdde2a82f2729d87b73c8e085c771a19727f7cc
Эдуард Шамсиев <s-6001@yandex.ru>
2021-12-17 13:54:31 +0500

    Доведение до рабочего состояния

--- task_15/Program.cs
+++ task_15/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace task_15
 {
@@ -10,34 +6,68 @@ namespace task_15
     {
         static void Main(string[] args)
         {
-
+            ArithProgression arithProgression = new ArithProgression(); //создаем объект класса
+            arithProgression.setStart(6);  //устанавлваем начальное значение
+            Console.WriteLine("Арифметическая прогрессия:");
+            Console.WriteLine(arithProgression.getNext());  //выводим следующее число ряда
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            arithProgression.reset();   //сбрасываем к начальному значению
+            Console.WriteLine();
+            GeomProgression geomProgression = new GeomProgression();    //создаем объект класса
+            geomProgression.setStart(6);    //устанавлваем начальное значение
+            Console.WriteLine("Геометрическая прогрессия:");
+            Console.WriteLine(geomProgression.getNext());   //выводим следующее число ряда
+            Console.WriteLine(geomProgression.getNext());
+            Console.WriteLine(geomProgression.getNext());
+            geomProgression.reset();    //сбрасываем к начальному значению
+            Console.ReadKey();
         }
-        interface ISeries
+    }
+    interface ISeries   //интерфейс
+    {
+        void setStart(int x);
+        int getNext();
+        void reset();
+    }
+    class ArithProgression : ISeries    //класс арифм прогрессии
+    {
+        int firstNumber;
+        int step = 3;
+        int next;
+        public void setStart(int x) //устанавливает начальное значение
         {
-            void setStart(int x);
-            int getNext();
-            void reset();
+            firstNumber = x;
+            next = x;
         }
-        class ArithProgression : ISeries
+        public int getNext()    //возвращает следующее число ряда
         {
-            int firstNumber = 2;
-            int step = 3;
-            int next;
-            public int getNext(int f, int s)
-            {
-                next = firstNumber+step;
-                return next;
-            }
-
-            public void reset()
-            {
-                throw new NotImplementedException();
-            }
-
-            public void setStart(int x)
-            {
-                
-            }
+            next = next + step;
+            return next;
+        }
+        public void reset() //выполняет сброс к начальному значению
+        {
+            next = firstNumber;
+        }
+    }
+    class GeomProgression : ISeries //класс геом прогрессии
+    {
+        int firstNumber;
+        int step = 3;
+        int next;
+        public void setStart(int x) //устанавливает начальное значение
+        {
+            firstNumber = x;
+            next = x;
+        }
+        public int getNext()    //возвращает следующее число ряда
+        {
+            next = next * step;
+            return next;
+        }
+        public void reset() //выполняет сброс к начальному значению
+        {
+            next = firstNumber;
         }
     }
 }
